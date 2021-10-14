6659673befd734800b920200d334c5d4056e46d5
Casper Bastholm <42714453+casperb123@users.noreply.github.com>
2021-10-14 14:55:36 +0200

    Updated assembly information

--- SynapseXUI/Properties/AssemblyInfo.cs
+++ SynapseXUI/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SynapseXUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Custom user interface for Synapse X")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SynapseXUI")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © Casper Bastholm 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
