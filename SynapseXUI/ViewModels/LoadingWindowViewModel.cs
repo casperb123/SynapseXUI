8d1d5f3ba5aecaf9796bf3fb7d006fa6f31e197b
Casper Bastholm <bastholm.casper@gmail.com>
2021-12-12 11:46:35 +0100

    The application now closes if you get an error during loading

--- SynapseXUI/ViewModels/LoadingWindowViewModel.cs
+++ SynapseXUI/ViewModels/LoadingWindowViewModel.cs
@@ -88,10 +88,12 @@ namespace SynapseXUI.ViewModels
             switch (Event)
             {
                 case SxLibBase.SynLoadEvents.UNKNOWN:
-                    PromptWindow.Show("Loading Synapse X", "An unknown error occured. Please open the official UI", PromptType.OK);
+                    PromptWindow.Show("Loading Synapse X", "An unknown error occured. Please try opening the application again", PromptType.OK);
+                    Environment.Exit(0);
                     break;
                 case SxLibBase.SynLoadEvents.NOT_LOGGED_IN:
                     PromptWindow.Show("Loading Synapse X", "You are not logged in to Synapse X. Please open the official UI", PromptType.OK);
+                    Environment.Exit(0);
                     break;
                 case SxLibBase.SynLoadEvents.NOT_UPDATED:
                     PromptWindow.Show("Loading Synapse X", "Synapse X is not currently updated. Please wait for an update to release", PromptType.OK);
@@ -99,12 +101,15 @@ namespace SynapseXUI.ViewModels
                     break;
                 case SxLibBase.SynLoadEvents.FAILED_TO_VERIFY:
                     PromptWindow.Show("Loading Synapse X", "Failed to verify data. Please open the official UI", PromptType.OK);
+                    Environment.Exit(0);
                     break;
                 case SxLibBase.SynLoadEvents.FAILED_TO_DOWNLOAD:
                     PromptWindow.Show("Loading Synapse X", "Failed to download data. Please check your anti-virus software", PromptType.OK);
+                    Environment.Exit(0);
                     break;
                 case SxLibBase.SynLoadEvents.UNAUTHORIZED_HWID:
                     PromptWindow.Show("Loading Synapse X", "Unauthorized Hwid detected Please open the official UI", PromptType.OK);
+                    Environment.Exit(0);
                     break;
                 case SxLibBase.SynLoadEvents.CHECKING_WL:
                     LoadingStatus = "Checking whitelist...";
